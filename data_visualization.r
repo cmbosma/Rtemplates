6fbf4a7a0d9a17bc092fee00f6e23c97bc6f772a
C.M. Bosma <bosma.colin@gmail.com>
2017-01-09 22:04:47 -0500

    Update data_visualization.r

--- data_visualization.r
+++ data_visualization.r
@@ -5,14 +5,12 @@ browseURL("http://www.statmethods.net/") # Quick-R website - great reference for
 browseURL("http://cran.r-project.org/web/packages/available_packages_by_name.html") # Available CRAN Packages by Name
 browseURL("http://cran.r-project.org/web/views/") # List of packages by category
 browseURL("https://google-styleguide.googlecode.com/svn/trunk/Rguide.xml") # Google's R Style Guide
-browseURL("https://github.com/daattali/colourpicker#install") # colourpicker github page
 
 ## Load Packages
 ## -----------------------
 if (!require(ggplot2)) {install.packages("ggplot2"); require(ggplot2)} # ggplot2: Data visualization package - useful for specialized visualization
 if (!require(lattice)) {install.packages("lattice"); require(lattice)} # lattice: package for visualizing multivariate data/analyses
 if (!require(ggvis)) {install.packages("ggvis"); require(ggvis)} 
-if (!require(colourpicker)) {install.packages("colourpicker"); require(colourpicker)} # Creates wizard for choosing colors for graphics 
 
 ## Load Data -----------------------
 
