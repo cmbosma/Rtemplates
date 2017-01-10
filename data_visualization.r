cfa715117943bec333098dd9769a140a261c0dd5
C.M. Bosma <bosma.colin@gmail.com>
2017-01-09 21:59:54 -0500

    Update data_visualization.r

--- data_visualization.r
+++ data_visualization.r
@@ -1,26 +1,23 @@
 ## Data Visualization [in progress]
 
-## Open Useful Websites or Documentation
-## -----------------------
+## Open Useful Websites or Documentation -----------------------
 browseURL("http://www.statmethods.net/") # Quick-R website - great reference for data analysis in R
 browseURL("http://cran.r-project.org/web/packages/available_packages_by_name.html") # Available CRAN Packages by Name
 browseURL("http://cran.r-project.org/web/views/") # List of packages by category
 browseURL("https://google-styleguide.googlecode.com/svn/trunk/Rguide.xml") # Google's R Style Guide
-# [insert more useful URLs for data visualization in R]
+browseURL("https://github.com/daattali/colourpicker#install") # colourpicker github page
 
 ## Load Packages
 ## -----------------------
 if (!require(ggplot2)) {install.packages("ggplot2"); require(ggplot2)} # ggplot2: Data visualization package - useful for specialized visualization
 if (!require(lattice)) {install.packages("lattice"); require(lattice)} # lattice: package for visualizing multivariate data/analyses
-if (!require(ggvis)) {install.packages("ggvis"); require(ggvis)} # ggvis:
+if (!require(ggvis)) {install.packages("ggvis"); require(ggvis)} 
+if (!require(colourpicker)) {install.packages("colourpicker"); require(colourpicker)} # Creates wizard for choosing colors for graphics 
 
-## Load Data
-## -----------------------
-library(mtcars)
-str(mtcars)
+## Load Data -----------------------
 
-## Base Graphics
-## -----------------------
+## Base Graphics -----------------------
 
-## ggplot2
-## -----------------------
+## ggplot2 -----------------------
+          
+
