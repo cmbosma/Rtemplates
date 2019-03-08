6deaf557be7152ead1154e1d4df251a409b2e3f3
C.M. Bosma <bosma.colin@gmail.com>
2019-03-07 19:49:35 -0500

    Added residualizing variables

--- var_computation.r
+++ var_computation.r
@@ -1,19 +1,21 @@
 ## Code for common variable computation 
 ## Colin Bosma
 
+## Reverse scoring
+# Note: subtract value one more than absolute value of all levels of variable 
+# For example, if abs value is 4, then subtract by 5
+
+df[,c("vec1", "vec2")] <- x-df[,c
+
+# Example using CAMS-R 
+data[,c("cams_r_2", "cams_r_6", "cams_r_7")] <- 5-data[,c("cams_r_2", "cams_r_6", "cams_r_7")]
+
 ## Rowise sum
 
 df$vec <- rowSums(df[c("vec1", "vec2")], na.rm = FALSE)
 
-# example using BDI-II:
-data$bdi_s1 <- rowSums(data[c("bdi_ii_1", "bdi_ii_2", "bdi_ii_3",
-                              "bdi_ii_4", "bdi_ii_5", "bdi_ii_6",
-                              "bdi_ii_7", "bdi_ii_8", "bdi_ii_9",
-                              "bdi_ii_10", "bdi_ii_11", "bdi_ii_12",
-                              "bdi_ii_13", "bdi_ii_14", "bdi_ii_15",
-                              "bdi_ii_16", "bdi_ii_17", "bdi_ii_18",
-                              "bdi_ii_19", "bdi_ii_20", "bdi_ii_21")],
-                       na.rm = FALSE)
+# example using ERQ suppression subscale score:
+data$erq_sup <- rowSums(data[c("erq_2", "erq_4", "erq_6", "erq_9")], na.rm = FALSE)
 
 ## Rowise means 
 
@@ -39,3 +41,23 @@ happy_vas_sd <- data %>%
 data[ , "happy_vas_sd"] <- happy_vas_sd
 class(data$happy_vas_sd)
 summary(data$happy_vas_sd) 
+                         
+                               
+## Creating resdidualized variables
+
+# Build linear model to pull residuals from 
+# If longitudinal, DV = time 2
+new_mod <- lm(time2_vec ~ time1_vec, data = df, na.action = na.exclude)
+ls(new_mod) # list objects in model
+df$resid_vec <- resid(new_mod)                              
+
+# Example                              
+# Build linear model to pull residuals from
+panas_pa_model <- lm(panas_pos_tot_postmi ~ panas_pos_tot_baseline,
+                      data = data, na.action = na.exclude) # Keep missing values to keep dimensions of data frame
+ls(panas_pa_model) # list of objects in model
+data$panas_pa_react_resid <- resid(panas_pa_model) # assign to new var
+describe(data$panas_pa_react_resid) # checking work
+                               
+  
+
