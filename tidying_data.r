de27c1c6e23761113834c37ffae204b05b754b11
C.M. Bosma <bosma.colin@gmail.com>
2019-03-11 17:18:56 -0400

    update

--- tidying_data.r
+++ tidying_data.r
@@ -5,6 +5,7 @@
 ## LIbraries
 
 library(tidyverse)
+library(dplyr) # if needed
 
 ## Formatting data frame
 names(data) <- tolower(names(data)) # Change all variable names to lowercase
@@ -34,6 +35,7 @@ print() # print entire data set to console - only use with small data sets
 View() # see whole data set in separate tab
 
 ## Visualizing raw data
+## -----------------------------------------------------------------------------
 
 hist(data.frame$var) # view  histogram of single variable
 
@@ -42,6 +44,7 @@ plot() # view plot of two variables
 ggplot(df, mapping = aes()) # basic plotting from ggplot2
 
 ## Cleaning data
+## -----------------------------------------------------------------------------
 
 # renaming elements of vectors
 names(df)[names(df) == 'old.var.name'] <- 'new.var.name'
@@ -49,3 +52,23 @@ names(df)[names(df) == 'old.var.name'] <- 'new.var.name'
 # Remove a case and assign to new data frame
 newdf <- olddf[!(olddf$var=="case_string"),]
 
+## Computing Variables
+## -----------------------------------------------------------------------------
+
+# Summing by row
+
+df$vec <- rowSums(df[c("vec1", "vec2")], na.rm = FALSE)
+
+# example:
+data$bdi_s1 <- rowSums(data[c("bdi_ii_1", "bdi_ii_2", "bdi_ii_3",
+                              "bdi_ii_4", "bdi_ii_5", "bdi_ii_6",
+                              "bdi_ii_7", "bdi_ii_8", "bdi_ii_9",
+                              "bdi_ii_10", "bdi_ii_11", "bdi_ii_12",
+                              "bdi_ii_13", "bdi_ii_14", "bdi_ii_15",
+                              "bdi_ii_16", "bdi_ii_17", "bdi_ii_18",
+                              "bdi_ii_19", "bdi_ii_20", "bdi_ii_21")],
+                       na.rm = FALSE)
+
+# Row means 
+
+df$vec <- rowMean(df[c("vec1", "vec2")], na.rm = FALSE)
