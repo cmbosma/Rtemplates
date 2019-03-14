e97feb881662c084f446ca9e0bf743589fa1d5c7
C.M. Bosma <bosma.colin@gmail.com>
2019-03-14 00:10:36 -0400

    Added code for haven, data.table, and readxl pkgs

--- importing_exporting.r
+++ importing_exporting.r
@@ -5,7 +5,13 @@
 ## Packages
 
 if (!require(readr)) {install.packages("readr"); require(readr)} # package for importing data with additional options and control
+if (!require(data.table)) {install.packages("data.table"); require(data.table)} # package for fread() function
+if (!require(haven)) {install.packages("haven"); require(haven)} # package for fread() function
+if (!require(readxl)) {install.packages("readxl"); require(readxl)} # package for importing excel files
 
+
+## Importing 
+## ---------------------------------------------------------------------------------
 
 ## Importing .csv using base
 
@@ -19,12 +25,48 @@ data <- read.csv2("datafram.csv", dec = ",") # Includes useful arguement to allo
 
 ## Importing .csv using readr package
 
-# Note. This package allows you to designate the type of variable upon import (see arguments)
+# Notes. 
+# This package allows you to designate the type of variable upon import (see arguments)
+# The data is imported as a tibble                    
+  
 
 data <- file.path("[insert directory]")
 data <- read_csv("dataframe.csv")
 data <- read_csv(file.choose()) # Opens finder for you to choose the file
 
+## Importing .csv using fread() function from the data.table package
+
+# Notes: 
+# The fread() function does a good job guessing the format of your data
+# the data is imported as a data.table and data frame
+                     
+data <- fread("filename.csv")
+data <- data.table::fread("filename.csv") 
+                     
+# Can specify which columns and rows
+fread("path/to/file.txt", drop = 2:4)
+fread("path/to/file.txt", select = c(1, 5))
+fread("path/to/file.txt", drop = c("b", "c", "d"))
+fread("path/to/file.txt", select = c("a", "e"))
+
+## Importing .sav files (SPSS) using haven
+
+data <- read_sav("filename.sav")        
+data <- haven::read_sav("filename.sav")
+                     
+## Importing excel using readxl package
+# Notes: Accepts both .xls and .xlsm files
+                     
+excel_sheets() # lists sheets                     
+read_excel() # imports data. 
+                     
+data <- read_excel("path/to/file.xlsm")  #  Imports first sheet by default. 
+data <- read_excel("path/to/file.xlsm", sheet = 2) # imports second sheet                 
+data <- read_excel("path/to/file.xlsm", sheet = "name_of_sheet") # imports based on name of sheet                 
+
+                     
+## Exporting 
+## ---------------------------------------------------------------------------------
 ## Exporting .csv using base
 
 setwd("[insert directory") # Set working directory to project folder
