0523d13cb6e0d87d0793cb62c011056c84dcd420
C.M. Bosma <bosma.colin@gmail.com>
2020-01-10 11:10:03 -0800

    created utilities and maintenance script

---
+++ utilities_maintenance.r
@@ -0,0 +1,20 @@
+## R Script for Utilities and Maintenance 
+## -------------------------------------------------------
+
+## Maintenance -------------------------------------------
+
+## Package Maintence
+
+old.packages() # checks which installed packages can be updated
+
+update.packages(ask = FALSE) # updates all packages. Does not ask permission for each with 'ask argument'
+
+remove.packages() # removes packages
+
+## Utilities ---------------------------------------------
+
+## Automatic Styling
+
+# styler package adds a 
+library(styler)
+
