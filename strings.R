ec953bcc7406af68ec634ea29277d6b35692cdda
C.M. Bosma <bosma.colin@gmail.com>
2020-07-23 18:19:35 -0700

    updated strings template

--- strings.R
+++ strings.R
@@ -1,2 +1,23 @@
 ## Working With Strings
 
+# addins
+regexplain() # generates UI for creating and testing regular expressions 
+
+# Libraries
+library(stringr)
+library(rebus) # Package for an alternative way to implementing regular expressions
+
+# Paste
+
+paste() # sep default " " 
+paste0() # sep default ""
+
+# Stringr
+
+str_c() # sep default ""
+str_view() # shows in html the pattern you generated
+
+## Regular expressions
+
+# Alternation
+  # matchat pattern a or b
