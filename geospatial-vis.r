0387530555d23e58f90b1f6640ef9ef838ef5c8f
C.M. Bosma <bosma.colin@gmail.com>
2020-07-31 12:41:13 -0700

    Added basic ggmap usage

--- geospatial-vis.r
+++ geospatial-vis.r
@@ -1,26 +1,35 @@
 ## Geo Spacial Visualizations
 ## Notes based on Datacamp's Geo Visualization R Course 
 
+## GOOGLE API KEY
+## ---------------------------------------------------------
+
+# Need to get Google API key first
+
+browseURL("https://developers.google.com/maps/documentation/embed/get-api-key")
+
+register_google(key = "<key_hash>")
+
 ## PACKAGES
 ## ---------------------------------------------------------
 
-# Packages needed
-packages <- c("tidyverse",
-             "ggmap", # allows geo spatial data to be layered onto ggplot
-             )
-
-# Install packages not yet installed
-installed_packages <- packages %in% rownames(installed.packages())
-if (any(installed_packages == FALSE)) {
-  install.packages(packages[!installed_packages])
-}
-
-# Load packages into workspace
-invisible(lapply(packages, library, character.only = TRUE))
+library(tidyverse)
+library(ggmap)
 
 ## 
 ## ---------------------------------------------------------
 
 ## ggmap
-# get_map() location can be an object with a longitude and latitude pair
-get_map(location, zoom = , scale = )
+
+# Set location (can do within get_map) 
+loc <- c(lon = <longitude>, lat = <latitude>)
+
+# get_map() location can be an object with a longitude and latitude pair. 
+map <- get_map(location, zoom = , scale = )
+
+# Check map
+ggmap(map)
+
+# Basic plot of coordinates layered over map
+ggmap(map) + 
+  geom_point(aes(longitude_vec, latitude_vec), data = df)
