74dbdec664776f2aa57780e639913f95b119d74a
C.M. Bosma <bosma.colin@gmail.com>
2020-08-16 11:59:23 -0700

    Added additional ggmap arguments

--- geospatial-vis.r
+++ geospatial-vis.r
@@ -20,16 +20,38 @@ library(ggmap)
 ## ---------------------------------------------------------
 
 ## ggmap
+## Note
+  # Can specify different types of google maps, not just terrain map
 
 # Set location (can do within get_map) 
-loc <- c(lon = <longitude>, lat = <latitude>)
+location <- c(lon = <longitude>, lat = <latitude>)
 
 # get_map() location can be an object with a longitude and latitude pair. 
+# Check arguements. Can change map type. 
+# Basic
 map <- get_map(location, zoom = , scale = )
+# With useful arguements
+map <- get_map(location, zoom = , scale = , maptype = , source = )
 
 # Check map
-ggmap(map)
+ggmap(map, zoom = )
 
 # Basic plot of coordinates layered over map
 ggmap(map) + 
   geom_point(aes(longitude_vec, latitude_vec), data = df)
+
+# Specify default data aesthetics 
+# Can use ggmap background with ggplot2 specifications
+# Use baselayer arguement 
+ggmap(map, base_layer = ggplot(df, aes(lon, lat)) + 
+   geom_point()
+      
+## Can plot using faceting
+ggmap(map, base_layer = ggplot(df, aes(lon, lat)) + 
+   geom_point(aes(color = var) + 
+   facet_wrap(~ var)
+              
+              
+              
+                               
+
